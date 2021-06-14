bc2ae89f6bd809540c44774800cfcab60cd03692
tadosalvo <tadosalvo@users.noreply.github.com>
2021-06-13 19:11:56 -0500

    txt added to homepage. api access in progress

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,20 +1,29 @@
-import React,{useState} from 'react'
+import axios from 'axios'
+import React,{useState , useEffect} from 'react'
+
 
 const Homescreen = () => {
     console.log("hello")
     const [title,setTitle] = useState("this is my homescreen")
-    const [data,setData] = useState([])
-    
+    const [data,setData] = useState("")
 
+    useEffect( () => {
+        axios.get('https://api.pokemontcg.io/v2/cards').then(
+            response => {
+                setData(response.data.name); 
+            }
+        )
 
-
-
+    }, []);
 
 
     return (
         <div>
             <h1>{title}</h1>
+            {data}
         </div>
+
+        //<button onClick = {() => {data}}>GET DATA</button>
     )
 }
 
