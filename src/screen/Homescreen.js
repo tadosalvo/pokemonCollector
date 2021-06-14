f084aeb2120d10fc3c5fc694b7c6339edfd975b7
tadosalvo <tadosalvo@users.noreply.github.com>
2021-06-14 17:19:48 -0500

    added api with header key functionality

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,13 +1,49 @@
 import axios from 'axios'
 import React,{useState , useEffect} from 'react'
 
+//const [title] = useState("List of Pokemon")
+//const [data,setData] = useState([])
+
+
+// test api if pokemon is not working
+//https://www.boredapi.com/api/activity
+
+// fetches data from api
+const fetchData = () => {
+    return axios.get('https://api.pokemontcg.io/v2/cards', {
+        headers: {'X-Api-Key' : process.env.REACT_APP_API_KEY}})
+    .then( (res) => {
+        //const {response} = res;
+        console.log(res.data);
+        //return response;
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+}
+
 
 const Homescreen = () => {
-    const [title] = useState("List of Pokemon")
     const [data,setData] = useState([])
 
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+return (
+    <div>
+        <h1>Hello</h1>
+    </div>
+
+)
+
+
+
+
+    /*
     useEffect( () => {
-        axios.get('https://api.pokemontcg.io/v2/cards'/*, {'X-Api-Key' : 111}*/).then(
+        axios.get('https://api.pokemontcg.io/v2/cards', {'X-Api-Key' : 111}).then(
             response => {
                 setData(response.data.data); 
             }
@@ -26,6 +62,8 @@ const Homescreen = () => {
         </div>
 
     )
+    */
+    
 
 }
 
