443682ff44a4c436a8b8ca7eaa006541db3cc749
tadosalvo <tadosalvo@users.noreply.github.com>
2021-06-15 15:31:17 -0500

    commit

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -1,44 +1,40 @@
 import axios from 'axios'
 import React,{useState , useEffect} from 'react'
 
-//const [title] = useState("List of Pokemon")
-//const [data,setData] = useState([])
 
+const Homescreen = (props) => {
 
-// test api if pokemon is not working
-//https://www.boredapi.com/api/activity
-
-// fetches data from api
-const fetchData = () => {
-    return axios.get('https://api.pokemontcg.io/v2/cards', {
-        headers: {'X-Api-Key' : process.env.REACT_APP_API_KEY}})
-    .then( (res) => {
-        //const {response} = res;
-        console.log(res.data);
-        //return response;
-    })
-    .catch((err) => {
-        console.error(err);
-    })
-}
-
-
-const Homescreen = () => {
+    const [title,setTitle] = useState("this is my homescreen")
     const [data,setData] = useState([])
+    const [loading, setLoading] = useState(false)
 
+    const fetchData = async () => {
+        setLoading(true)
+        const options = {
+            headers: {'X-Api-Key' : "7719d04c-b2bf-44a2-a4b3-4d73ffc661d7" }
+        }
 
-    useEffect(() => {
+          const response = await axios.get("https://api.pokemontcg.io/v2/cards", options)
+          const responseData = response.data.data
+          setTitle(responseData[0].name)
+          setData(responseData)
+          setLoading(false) 
+
+    }
+
+    useEffect( () => {
         fetchData();
-    }, [])
-
-return (
-    <div>
-        <h1>Hello</h1>
-    </div>
-
-)
+        console.log(data, "this is data")
+        props.thisFunction();
+    }, []);
 
 
+    return (
+        <div>
+            {!loading ?<h1>{title}</h1> : <h1>{props.name}</h1>}
+        </div>
+
+    )
 
 }
 
