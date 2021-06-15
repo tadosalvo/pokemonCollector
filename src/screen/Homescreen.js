fa338987c8068171e58f73eadace5ffd0277bf9b
Jethro Talavera <jethro.talavera1@gmail.com>
2021-06-15 15:56:12 -0400

    fixed conflict

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -4,6 +4,31 @@ import React,{useState , useEffect} from 'react'
 
 const Homescreen = () => {
 
+    const [title,setTitle] = useState("this is my homescreen")
+    const [data,setData] = useState([])
+
+    const fetchData = async () => {
+       
+          const response = await axios.get("https://api.pokemontcg.io/v2/cards")
+          const responseData = response.data.data
+          setTitle(responseData[0].name)
+          console.log(responseData[0].name)
+
+    }
+
+    useEffect( () => {
+        fetchData();
+        console.log(data)
+    }, []);
+
+
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+
+    )
+
 }
 
 export default Homescreen
