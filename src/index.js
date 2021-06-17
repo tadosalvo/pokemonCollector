08247c1252a9575554d563e7523df9721f4741cc
tadosalvo <tadosalvo@users.noreply.github.com>
2021-06-17 14:08:54 -0500

    added redux template in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+
+
+//STORE
+
+//Action
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+//Reducer
+const counter = (state = 0,action = {}) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return 0;
+  }
+}
+
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//Dispatch
+store.dispatch(increment());
+
 
 ReactDOM.render(
   <React.StrictMode>
