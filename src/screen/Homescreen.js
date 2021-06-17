e61984d0ddcac099bc310a154ba98eecc9feefe2
tadosalvo <tadosalvo@users.noreply.github.com>
2021-06-17 11:18:59 -0500

    removed comment

--- src/screen/Homescreen.js
+++ src/screen/Homescreen.js
@@ -10,9 +10,7 @@ const Homescreen = (props) => {
 
 
     return (
-        // bug where it shows name in prior state then in current state rather than
-        // <h1>test</h1>
-        // Example: Goes from "Title" in App.js then to responseDate[1].name or "Bulbasaur"
+
         <div> 
             {!props.loading ? <h1>{props.name}</h1> : <h1>loading...</h1>}
         </div>
