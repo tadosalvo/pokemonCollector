48bd041354674f5158655e87502404ebdc1a8e20
tadosalvo <tadosalvo@users.noreply.github.com>
2021-06-24 22:29:03 -0500

    reverted appbar changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import React from 'react'
 import Homescreen from "./screen/Homescreen"
 import AboutScreen from './screen/AboutScreen'
-import SearchAppBar from './components/appBar';
+import ButtonAppBar from './components/appBar';
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
    
       <Router>
         <main >
-            <SearchAppBar/>         
+            <ButtonAppBar/>         
             <Route exact path="/" component={Homescreen} />
         </main>
       </Router>
