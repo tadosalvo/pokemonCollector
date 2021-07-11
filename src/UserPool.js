b5b4b3e0062621cd3934c0f85f278ee1ae7c639d
tadosalvo <tadosalvo@users.noreply.github.com>
2021-07-11 13:31:04 -0500

    fixed gitignore UserPool

--- src/UserPool.js
+++
@@ -1,8 +0,0 @@
-import {CognitoUserPool} from 'amazon-cognito-identity-js'
-
-const poolData = {
-    UserPoolId: "us-east-2_i8Jpgsf1L",
-    ClientId : "247e050q5lborh1ial9sovvudq"
-}
-
-export default new CognitoUserPool(poolData)
